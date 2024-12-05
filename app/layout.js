69f35aa0c400628e1eed74d294af24b16e816d7f
dixitt_ji <2024eb02196@online.bits-pilani.ac.in>
2024-12-05 05:46:19 +0530

    added logo and favicon

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <title>Feel Mitra - Your Emotional Companion</title>
-        <link rel="icon" href="/cat.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body suppressHydrationWarning>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
